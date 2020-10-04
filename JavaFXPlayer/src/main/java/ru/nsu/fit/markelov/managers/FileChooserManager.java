9da8449ea612bbeb4bf9684516cdeaf8e9e0b2e2
omarkelov <36790281+omarkelov@users.noreply.github.com>
2020-10-04 17:30:17 +0700

    #3 added FileChooserManager

---
+++ JavaFXPlayer/src/main/java/ru/nsu/fit/markelov/managers/FileChooserManager.java
@@ -0,0 +1,68 @@
+package ru.nsu.fit.markelov.managers;
+
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import ru.nsu.fit.markelov.util.validation.IllegalInputException;
+
+import java.io.File;
+
+import static ru.nsu.fit.markelov.util.validation.IllegalInputException.requireNonNull;
+
+/**
+ * FileChooserManager class is used for letting user to choose files from the open dialogs.
+ *
+ * @author Oleg Markelov
+ */
+public class FileChooserManager {
+
+    private final Stage stage;
+
+    private final FileChooser videoFileChooser;
+    private final FileChooser subtitlesFileChooser;
+
+    private File lastVideoDirectory;
+
+    /**
+     * Creates new FileChooserManager with specified JavaFX stage.
+     *
+     * @param stage JavaFX stage.
+     * @throws IllegalInputException if one of the input parameters is null.
+     */
+    public FileChooserManager(Stage stage) throws IllegalInputException {
+        this.stage = requireNonNull(stage);
+
+        videoFileChooser = new FileChooser();
+        videoFileChooser.setTitle("Choose Video");
+        subtitlesFileChooser = new FileChooser();
+        subtitlesFileChooser.setTitle("Choose Subtitles");
+
+        lastVideoDirectory = new File("F:\\Multimedia\\Series\\Westworld\\1\\"); // TODO -hardcode
+    }
+
+    /**
+     * Lets user to choose a video file.
+     *
+     * @return chosen video file.
+     */
+    public File chooseVideoFile() { // TODO only video
+        videoFileChooser.setInitialDirectory(lastVideoDirectory);
+
+        File file = videoFileChooser.showOpenDialog(stage);
+        if (file != null) {
+            lastVideoDirectory = new File(file.getParent());
+        }
+
+        return file;
+    }
+
+    /**
+     * Lets user to choose a subtitles file.
+     *
+     * @return chosen subtitles file.
+     */
+    public File chooseSubtitlesFile() { // TODO only .str
+        subtitlesFileChooser.setInitialDirectory(lastVideoDirectory);
+
+        return subtitlesFileChooser.showOpenDialog(stage);
+    }
+}
