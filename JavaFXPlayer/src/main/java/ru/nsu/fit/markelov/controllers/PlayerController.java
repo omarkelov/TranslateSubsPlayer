617a6c177bb6959884c05a6e5871d991f9252d8f
omarkelov <36790281+omarkelov@users.noreply.github.com>
2020-10-04 17:41:31 +0700

    #4 Menu bar basic implementation

--- JavaFXPlayer/src/main/java/ru/nsu/fit/markelov/controllers/PlayerController.java
+++ JavaFXPlayer/src/main/java/ru/nsu/fit/markelov/controllers/PlayerController.java
@@ -2,10 +2,15 @@ package ru.nsu.fit.markelov.controllers;
 
 import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.RadioMenuItem;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.text.TextFlow;
+import ru.nsu.fit.markelov.managers.FileChooserManager;
 import ru.nsu.fit.markelov.managers.SceneManager;
 import ru.nsu.fit.markelov.subtitles.BOMSrtParser;
 import ru.nsu.fit.markelov.subtitles.JavaFxSubtitles;
@@ -13,11 +18,13 @@ import ru.nsu.fit.markelov.util.validation.IllegalInputException;
 import uk.co.caprica.vlcj.factory.MediaPlayerFactory;
 import uk.co.caprica.vlcj.player.base.MediaPlayer;
 import uk.co.caprica.vlcj.player.base.MediaPlayerEventAdapter;
+import uk.co.caprica.vlcj.player.base.TrackDescription;
 import uk.co.caprica.vlcj.player.embedded.EmbeddedMediaPlayer;
 import uk.co.caprica.vlcj.subs.Spus;
 import uk.co.caprica.vlcj.subs.handler.SpuHandler;
 import uk.co.caprica.vlcj.subs.parser.SpuParseException;
 
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
@@ -38,77 +45,58 @@ public class PlayerController implements Controller {
     @FXML private GridPane gridPane;
     @FXML private TextFlow textFlow;
 
-    private final SceneManager sceneManager;
+    @FXML private Menu audioMenu;
+    @FXML private Menu subtitlesMenu;
+    @FXML private MenuItem fileOpenItem;
+    @FXML private MenuItem fileCloseItem;
+    @FXML private MenuItem subtitlesAddItem;
+    @FXML private MenuItem helpAboutItem;
 
-    private SpuHandler spuHandler;
+    private final ToggleGroup audioToggleGroup = new ToggleGroup();
+    private final ToggleGroup subtitlesToggleGroup = new ToggleGroup();
+
+    private final SceneManager sceneManager;
+    private final FileChooserManager fileChooserManager;
+
+    private File videoFile;
+
+    private SpuHandler subtitlesHandler;
     private final MediaPlayerFactory mediaPlayerFactory;
     private final EmbeddedMediaPlayer embeddedMediaPlayer;
+
+    private boolean initialized = false;
 
     /**
      * Creates new PlayerController with specified SceneManager.
      *
-     * @param sceneManager scene manager.
+     * @param sceneManager       scene manager.
+     * @param fileChooserManager file chooser manager.
      * @throws IllegalInputException if one of the input parameters is null.
      */
-    public PlayerController(SceneManager sceneManager) throws IllegalInputException {
+    public PlayerController(SceneManager sceneManager,
+                            FileChooserManager fileChooserManager) throws IllegalInputException {
         this.sceneManager = requireNonNull(sceneManager);
+        this.fileChooserManager = requireNonNull(fileChooserManager);
 
         mediaPlayerFactory = new MediaPlayerFactory();
         embeddedMediaPlayer = mediaPlayerFactory.mediaPlayers().newEmbeddedMediaPlayer();
 
-        Spus spus;
-        try (FileReader fileReader = new FileReader("F:\\Multimedia\\Series\\Westworld\\1\\Subtitles\\Westworld.[S01E01].HD720.DUB.[qqss44].eng.srt")) {
-            spus = new BOMSrtParser().parse(fileReader);
-        } catch (IOException|SpuParseException|RuntimeException e) { // TODO
-            e.printStackTrace();
-            spus = null;
-        }
-
-        if (spus != null) {
-            spuHandler = new SpuHandler(spus);
-//            spuHandler.setOffset(50);
-            spuHandler.addSpuEventListener(spu -> {
-                if (spu != null) {
-                    JavaFxSubtitles javaFxSubtitles = new JavaFxSubtitles(spu.value().toString());
-                    Platform.runLater(() -> javaFxSubtitles.updateTextFlow(textFlow));
-                } else {
-                    Platform.runLater(() -> textFlow.getChildren().clear());
-                }
-            });
-        } else {
-            System.out.println("Could not find any subtitles in the provided file.");
-        }
-
         embeddedMediaPlayer.events().addMediaPlayerEventListener(new MediaPlayerEventAdapter() {
             @Override
             public void playing(MediaPlayer mediaPlayer) {
-                mediaPlayer.submit(() -> {
-                    mediaPlayer.audio().setTrack(2);
-                    mediaPlayer.subpictures().setTrack(-1);
-//                    mediaPlayer.controls().setTime(3 * 60 * 1000 + 20 * 1000);
-                    mediaPlayer.controls().setTime(2 * 60 * 1000 + 47 * 1000);
-//                    mediaPlayer.controls().setPosition(0.3f);
-
-                    /*for (TrackDescription trackDescription : mediaPlayer.audio().trackDescriptions()) {
-                        System.out.println(trackDescription.toString());
-                    }*/
-                });
+                initPlayingIfNot(mediaPlayer);
             }
 
             @Override
             public void timeChanged(MediaPlayer mediaPlayer, long newTime) {
-                if (spuHandler != null) {
-                    spuHandler.setTime(newTime);
+                if (subtitlesHandler != null) {
+                    subtitlesHandler.setTime(newTime);
                 }
-
-                /*if (newTime > 2 * 60 * 1000 + 0 * 1000) {
-                    mediaPlayer.submit(() -> mediaPlayer.controls().setTime(1 * 60 * 1000 + 47 * 1000));
-                }*/
             }
 
             @Override
             public void error(MediaPlayer mediaPlayer) {
-                System.out.println("----- ERROR -----");
+                System.out.println("----- ERROR -----"); // todo show message
             }
         });
     }
@@ -120,9 +108,141 @@ public class PlayerController implements Controller {
         videoImageView.fitWidthProperty().bind(root.widthProperty());
         videoImageView.fitHeightProperty().bind(root.heightProperty());
 
-        embeddedMediaPlayer.media().play("F:\\Multimedia\\Series\\Westworld\\1\\Westworld.[S01E01].HD720.DUB.[qqss44].mkv");
-//        embeddedMediaPlayer.media().play("F:\\Multimedia\\Films\\Collection\\Death.Proof.2007.BluRay.1080p.DTS.x264.dxva-EuReKA.mkv");
-//        embeddedMediaPlayer.media().play("F:\\Multimedia\\Films\\Collection\\Baby.Driver.2017.BDRip.1.46Gb.Dub.MegaPeer.avi");
+        fileOpenItem.setOnAction(actionEvent -> {
+            embeddedMediaPlayer.controls().setPause(true);
+
+            File file = fileChooserManager.chooseVideoFile();
+            if (file != null) {
+                disposePlaying();
+
+                embeddedMediaPlayer.media().play(file.getAbsolutePath());
+
+                videoFile = file;
+            }
+
+            embeddedMediaPlayer.controls().setPause(false);
+        });
+
+        fileCloseItem.setOnAction(actionEvent -> disposePlaying());
+    }
+
+    private void initPlayingIfNot(MediaPlayer mediaPlayer) {
+        if (!initialized) {
+            initAudioMenu(mediaPlayer);
+            initSubtitlesMenu(mediaPlayer);
+            Platform.runLater(() -> sceneManager.setTitle(videoFile.getName()));
+
+            initialized = true;
+
+            mediaPlayer.submit(() -> { // TODO -hardcode
+                mediaPlayer.controls().setPosition(0.042569723f);
+                //mediaPlayer.controls().setTime(2 * 60 * 1000 + 47 * 1000);
+            });
+        }
+    }
+
+    private void disposePlaying() {
+        embeddedMediaPlayer.controls().stop();
+        videoImageView.setImage(null);
+
+        disposeAudioMenu();
+        disposeSubtitlesMenu();
+        disposeSubtitles();
+
+        sceneManager.setDefaultTitle();
+        videoFile = null;
+
+        initialized = false;
+    }
+
+    private void initAudioMenu(MediaPlayer mediaPlayer) {
+        for (TrackDescription trackDescription : mediaPlayer.audio().trackDescriptions()) {
+            if (trackDescription.id() == -1) {
+                continue;
+            }
+
+            RadioMenuItem radioItem = new RadioMenuItem(trackDescription.description());
+            radioItem.setToggleGroup(audioToggleGroup);
+            radioItem.setSelected(trackDescription.id() == mediaPlayer.audio().track());
+            radioItem.setOnAction(actionEvent -> mediaPlayer.audio().setTrack(trackDescription.id()));
+            audioMenu.getItems().add(radioItem);
+        }
+
+        audioMenu.setDisable(false);
+    }
+
+    private void disposeAudioMenu() {
+        audioMenu.setDisable(true);
+        audioMenu.getItems().clear();
+        audioToggleGroup.getToggles().clear();
+    }
+
+    private void initSubtitlesMenu(MediaPlayer mediaPlayer) {
+        mediaPlayer.subpictures().setTrack(-1); // disable subtitles inside vlc-player
+
+        RadioMenuItem disabledRadioItem = new RadioMenuItem("Disabled");
+        disabledRadioItem.setToggleGroup(subtitlesToggleGroup);
+        disabledRadioItem.setSelected(true);
+        disabledRadioItem.setOnAction(actionEvent -> disposeSubtitles());
+        subtitlesMenu.getItems().add(disabledRadioItem);
+
+        MenuItem subtitlesOpenItem = new MenuItem("Open .srt-file");
+        subtitlesOpenItem.getStyleClass().add("italic");
+        subtitlesOpenItem.setOnAction(actionEvent -> {
+            embeddedMediaPlayer.controls().setPause(true);
+
+            File file = fileChooserManager.chooseSubtitlesFile();
+            if (file != null) {
+                RadioMenuItem radioMenuItem = new RadioMenuItem(file.getName());
+                radioMenuItem.setToggleGroup(subtitlesToggleGroup);
+                radioMenuItem.setSelected(true);
+                radioMenuItem.setOnAction(fileActionEvent -> initSubtitles(file.getAbsolutePath()));
+                subtitlesMenu.getItems().add(subtitlesMenu.getItems().size() - 1, radioMenuItem);
+
+                initSubtitles(file.getAbsolutePath());
+            }
+
+            embeddedMediaPlayer.controls().setPause(false);
+        });
+        subtitlesMenu.getItems().add(subtitlesOpenItem);
+
+        // todo walk directory for subtitles
+        /*for (TrackDescription trackDescription : mediaPlayer.subpictures().trackDescriptions()) { // todo delete
+            System.out.println(trackDescription.toString());
+        }*/
+
+        subtitlesMenu.setDisable(false);
+    }
+
+    private void disposeSubtitlesMenu() {
+        subtitlesMenu.setDisable(true);
+        subtitlesMenu.getItems().clear();
+        subtitlesToggleGroup.getToggles().clear();
+    }
+
+    private void initSubtitles(String fileName) {
+        try (FileReader fileReader = new FileReader(fileName)) {
+            Spus subtitleUnits = new BOMSrtParser().parse(fileReader);
+
+            subtitlesHandler = new SpuHandler(subtitleUnits);
+            //subtitlesHandler.setOffset(50);
+            subtitlesHandler.addSpuEventListener(subtitleUnit -> {
+                if (subtitleUnit != null) {
+                    JavaFxSubtitles javaFxSubtitles = new JavaFxSubtitles(subtitleUnit.value().toString());
+                    Platform.runLater(() -> javaFxSubtitles.updateTextFlow(textFlow));
+                } else {
+                    Platform.runLater(() -> textFlow.getChildren().clear());
+                }
+            });
+        } catch (IOException|SpuParseException|RuntimeException e) { // TODO show message
+            System.out.println("Could not find any subtitles in the provided file.");
+            e.printStackTrace();
+        }
+    }
+
+    private void disposeSubtitles() {
+        subtitlesHandler = null;
+        textFlow.getChildren().clear();
     }
 
     /**
