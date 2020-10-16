db5de03067d7b32eee02578e3b74cdbc48c75129
omarkelov <36790281+omarkelov@users.noreply.github.com>
2020-10-17 00:52:21 +0700

    #4 Created MenuBarControl and moved code from PlayerController

--- JavaFXPlayer/src/main/java/ru/nsu/fit/markelov/controllers/PlayerController.java
+++ JavaFXPlayer/src/main/java/ru/nsu/fit/markelov/controllers/PlayerController.java
@@ -8,9 +8,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuItem;
-import javafx.scene.control.RadioMenuItem;
 import javafx.scene.control.Slider;
-import javafx.scene.control.ToggleGroup;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCodeCombination;
 import javafx.scene.input.KeyEvent;
@@ -21,6 +19,8 @@ import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.text.TextFlow;
 import ru.nsu.fit.markelov.controllers.player.ControlBarControl;
+import ru.nsu.fit.markelov.controllers.player.MenuBarControl;
+import ru.nsu.fit.markelov.controllers.player.MenuBarObserver;
 import ru.nsu.fit.markelov.controllers.player.SubtitlesControl;
 import ru.nsu.fit.markelov.controllers.player.SubtitlesObserver;
 import ru.nsu.fit.markelov.managers.FileChooserManager;
@@ -29,12 +29,9 @@ import ru.nsu.fit.markelov.util.validation.IllegalInputException;
 import uk.co.caprica.vlcj.factory.MediaPlayerFactory;
 import uk.co.caprica.vlcj.player.base.MediaPlayer;
 import uk.co.caprica.vlcj.player.base.MediaPlayerEventAdapter;
-import uk.co.caprica.vlcj.player.base.TrackDescription;
 import uk.co.caprica.vlcj.player.embedded.EmbeddedMediaPlayer;
 
 import java.io.File;
-import java.nio.file.Files;
-import java.nio.file.Path;
 
 import static javafx.scene.input.KeyCode.ENTER;
 import static javafx.scene.input.KeyCode.ESCAPE;
@@ -51,7 +48,7 @@ import static uk.co.caprica.vlcj.javafx.videosurface.ImageViewVideoSurfaceFactor
  *
  * @author Oleg Markelov
  */
-public class PlayerController implements Controller, SubtitlesObserver {
+public class PlayerController implements Controller, SubtitlesObserver, MenuBarObserver {
 
     private static final String FXML_FILE_NAME = "player.fxml";
 
@@ -70,13 +67,6 @@ public class PlayerController implements Controller, SubtitlesObserver {
     @FXML private Group translationGroup;
     @FXML private TextFlow translationTextFlow;
 
-    @FXML private Menu audioMenu;
-    @FXML private Menu subtitlesMenu;
-    @FXML private MenuItem fileOpenItem;
-    @FXML private MenuItem fileCloseItem;
-    @FXML private MenuItem subtitlesAddItem;
-    @FXML private MenuItem helpAboutItem;
-
     @FXML private Slider slider;
 
     @FXML private HBox leftControlBox;
@@ -92,13 +82,14 @@ public class PlayerController implements Controller, SubtitlesObserver {
     @FXML private Label currentTimeLabel;
     @FXML private Label entireTimeLabel;
 
-    private final ToggleGroup audioToggleGroup = new ToggleGroup();
-    private final ToggleGroup subtitlesToggleGroup = new ToggleGroup();
+    @FXML private Menu audioMenu;
+    @FXML private Menu subtitlesMenu;
+    @FXML private MenuItem fileOpenItem;
+    @FXML private MenuItem fileCloseItem;
+    @FXML private MenuItem helpAboutItem;
 
     private final SceneManager sceneManager;
     private final FileChooserManager fileChooserManager;
-
-    private File videoFile;
 
     private final MediaPlayerFactory mediaPlayerFactory;
     private final EmbeddedMediaPlayer embeddedMediaPlayer;
@@ -107,6 +98,7 @@ public class PlayerController implements Controller, SubtitlesObserver {
 
     private SubtitlesControl subtitlesControl;
     private ControlBarControl controlBarControl;
+    private MenuBarControl menuBarControl;
 
     /**
      * Creates new PlayerController with specified SceneManager.
@@ -135,11 +127,10 @@ public class PlayerController implements Controller, SubtitlesObserver {
                     Platform.runLater(() -> {
                         controlBarControl.onStopPressed();
                         mediaPlayer.subpictures().setTrack(-1);
-                        mediaPlayer.audio().setTrack(
-                            (Integer) audioToggleGroup.getSelectedToggle().getUserData());
+                        mediaPlayer.audio().setTrack(menuBarControl.getSelectedAudioTrack());
                     });
                 } else {
-                    Platform.runLater(() -> initPlayingIfNot(mediaPlayer));
+                    Platform.runLater(() -> initPlayingIfNot());
                 }
             }
 
@@ -168,10 +159,6 @@ public class PlayerController implements Controller, SubtitlesObserver {
 
         videoImageView.fitWidthProperty().bind(root.widthProperty());
         videoImageView.fitHeightProperty().bind(root.heightProperty());
-
-        fileOpenItem.setOnAction(actionEvent -> chooseFileAndPlay());
-        fileCloseItem.setOnAction(actionEvent -> disposePlaying());
-
         videoImageView.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent -> subtitlesControl.hideTranslationBar());
 
         subtitlesControl = new SubtitlesControl(sceneManager, this, subtitlesGroup,
@@ -180,6 +167,9 @@ public class PlayerController implements Controller, SubtitlesObserver {
         controlBarControl = new ControlBarControl(sceneManager, embeddedMediaPlayer,
             subtitlesControl, slider, leftControlBox, centerControlBox, pauseButton, stopButton,
             skipLeftButton, skipRightButton, soundButton, expandButton, currentTimeLabel, entireTimeLabel);
+
+        menuBarControl = new MenuBarControl(this, fileChooserManager, embeddedMediaPlayer,
+            subtitlesControl, controlBarControl, audioMenu, subtitlesMenu, fileOpenItem, fileCloseItem);
     }
 
     @Override
@@ -187,6 +177,16 @@ public class PlayerController implements Controller, SubtitlesObserver {
         if (embeddedMediaPlayer.status().isPlaying()) {
             controlBarControl.onPausePressed(true);
         }
+    }
+
+    @Override
+    public void onFileClicked() {
+        chooseFileAndPlay();
+    }
+
+    @Override
+    public void onClosedClicked() {
+        disposePlaying();
     }
 
     private void chooseFileAndPlay() {
@@ -205,7 +205,7 @@ public class PlayerController implements Controller, SubtitlesObserver {
 
             embeddedMediaPlayer.media().play(file.getAbsolutePath());
 
-            videoFile = file;
+            menuBarControl.setVideoFile(file);
         }
 
         if (isPlaying) {
@@ -213,17 +213,15 @@ public class PlayerController implements Controller, SubtitlesObserver {
         }
     }
 
-    private void initPlayingIfNot(MediaPlayer mediaPlayer) {
+    private void initPlayingIfNot() {
         if (!initialized) {
             initialized = true;
 
-            sceneManager.setTitle(videoFile.getName());
+            sceneManager.setTitle(menuBarControl.getVideoFile().getName());
             controlBarControl.onPausePressed(false);
 
-            initAudioMenu(mediaPlayer);
-            initSubtitlesMenu(mediaPlayer);
-
             controlBarControl.init();
+            menuBarControl.init();
         }
     }
 
@@ -232,120 +230,16 @@ public class PlayerController implements Controller, SubtitlesObserver {
         embeddedMediaPlayer.controls().stop();
         videoImageView.setImage(null);
 
-        disposeAudioMenu();
-        disposeSubtitlesMenu();
         subtitlesControl.disposeSubtitles();
-
         controlBarControl.dispose();
+        menuBarControl.dispose();
 
         sceneManager.setDefaultTitle();
-        videoFile = null;
+        menuBarControl.setVideoFile(null);
 
         root.requestFocus();
 
         initialized = false;
-    }
-
-    private void initAudioMenu(MediaPlayer mediaPlayer) {
-        for (TrackDescription trackDescription : mediaPlayer.audio().trackDescriptions()) {
-            if (trackDescription.id() == -1) {
-                continue;
-            }
-
-            RadioMenuItem radioItem = new RadioMenuItem(trackDescription.description());
-            radioItem.setToggleGroup(audioToggleGroup);
-            radioItem.setSelected(trackDescription.id() == mediaPlayer.audio().track());
-            radioItem.setUserData(trackDescription.id());
-            radioItem.setOnAction(actionEvent -> mediaPlayer.audio().setTrack(trackDescription.id()));
-            radioItem.setMnemonicParsing(false);
-            audioMenu.getItems().add(radioItem);
-        }
-
-        audioMenu.setDisable(false);
-    }
-
-    private void disposeAudioMenu() {
-        audioMenu.setDisable(true);
-        audioMenu.getItems().clear();
-        audioToggleGroup.getToggles().clear();
-    }
-
-    private void initSubtitlesMenu(MediaPlayer mediaPlayer) {
-        mediaPlayer.subpictures().setTrack(-1); // disable subtitles inside vlc-player
-
-        RadioMenuItem disabledRadioItem = new RadioMenuItem("Disabled");
-        disabledRadioItem.setToggleGroup(subtitlesToggleGroup);
-        disabledRadioItem.setSelected(true);
-        subtitlesControl.setCurrentSubtitlesMenuItem(disabledRadioItem);
-        disabledRadioItem.setOnAction(actionEvent -> {
-            subtitlesControl.disposeSubtitles();
-            subtitlesControl.setCurrentSubtitlesMenuItem(disabledRadioItem);
-        });
-        disabledRadioItem.setMnemonicParsing(false);
-        subtitlesMenu.getItems().add(disabledRadioItem);
-
-        MenuItem subtitlesOpenItem = new MenuItem("Open .srt-file");
-        subtitlesOpenItem.getStyleClass().add("italic");
-        subtitlesOpenItem.setOnAction(actionEvent -> {
-            boolean isPlaying = embeddedMediaPlayer.status().isPlaying();
-            if (isPlaying) {
-                controlBarControl.onPausePressed(true);
-            }
-
-            File file = fileChooserManager.chooseSubtitlesFile();
-            if (file != null) {
-                RadioMenuItem radioMenuItem = new RadioMenuItem(file.getName());
-                radioMenuItem.setToggleGroup(subtitlesToggleGroup);
-                radioMenuItem.setSelected(true);
-                radioMenuItem.setOnAction(fileActionEvent -> subtitlesControl.initSubtitles(file.getAbsolutePath(), radioMenuItem, (long) controlBarControl.getSliderValue()));
-                radioMenuItem.setMnemonicParsing(false);
-                subtitlesMenu.getItems().add(subtitlesMenu.getItems().size() - 1, radioMenuItem);
-
-                subtitlesControl.initSubtitles(file.getAbsolutePath(), radioMenuItem, (long) controlBarControl.getSliderValue());
-            }
-
-            if (isPlaying) {
-                controlBarControl.onPausePressed(false);
-            }
-        });
-        subtitlesMenu.getItems().add(subtitlesOpenItem);
-
-        try {
-            String videoFileName = videoFile.getName().replaceFirst("[.][^.]+$", "");
-            Path videoFilePath = Path.of(videoFile.getParent());
-            Files
-                .walk(videoFilePath)
-                .filter(path -> {
-                    String fileName = path.getFileName().toString();
-                    return fileName.startsWith(videoFileName) && fileName.endsWith(".srt");
-                })
-                .forEach(subtitlesPath -> {
-                    boolean disabled = ((RadioMenuItem) subtitlesToggleGroup.getSelectedToggle()).getText().equals("Disabled");
-
-                    RadioMenuItem radioMenuItem = new RadioMenuItem(videoFilePath.relativize(subtitlesPath).toString());
-                    radioMenuItem.setToggleGroup(subtitlesToggleGroup);
-                    radioMenuItem.setSelected(disabled);
-                    radioMenuItem.setOnAction(fileActionEvent -> subtitlesControl.initSubtitles(
-                        subtitlesPath.toString(), radioMenuItem, (long) controlBarControl.getSliderValue()));
-                    radioMenuItem.setMnemonicParsing(false);
-                    subtitlesMenu.getItems().add(subtitlesMenu.getItems().size() - 1, radioMenuItem);
-
-                    if (disabled) {
-                        subtitlesControl.initSubtitles(
-                            subtitlesPath.toString(), radioMenuItem, (long) controlBarControl.getSliderValue());
-                    }
-                });
-        } catch (Exception e) {
-            System.out.println("Could not walk current video file directory: " + e.getMessage());
-        }
-
-        subtitlesMenu.setDisable(false);
-    }
-
-    private void disposeSubtitlesMenu() {
-        subtitlesMenu.setDisable(true);
-        subtitlesMenu.getItems().clear();
-        subtitlesToggleGroup.getToggles().clear();
     }
 
     private void onKeyReleased(KeyEvent keyEvent) {
---
+++ JavaFXPlayer/src/main/java/ru/nsu/fit/markelov/controllers/player/MenuBarObserver.java
@@ -0,0 +1,6 @@
+package ru.nsu.fit.markelov.controllers.player;
+
+public interface MenuBarObserver {
+    void onFileClicked();
+    void onClosedClicked();
+}
