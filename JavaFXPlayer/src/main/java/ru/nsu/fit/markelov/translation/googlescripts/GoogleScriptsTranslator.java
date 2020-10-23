919b81e72d03641dbdc17d64eca9b6912bd2468f
omarkelov <36790281+omarkelov@users.noreply.github.com>
2020-10-23 14:47:04 +0700

    #17 Created GoogleScriptsTranslator

---
+++ JavaFXPlayer/src/main/java/ru/nsu/fit/markelov/translation/googlescripts/GoogleScriptsTranslator.java
@@ -0,0 +1,33 @@
+package ru.nsu.fit.markelov.translation.googlescripts;
+
+import ru.nsu.fit.markelov.translation.Translator;
+import ru.nsu.fit.markelov.translation.entities.TranslationResult;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import static ru.nsu.fit.markelov.util.http.HttpUtil.getContent;
+
+public class GoogleScriptsTranslator extends Translator {
+    // todo scripts from multiple emails
+
+    private static final String GOOGLE_SCRIPTS_URL_FORMAT = "https://script.google.com/macros/s/" +
+        "AKfycbzEbZdkPbqj7IrDD1kooNU_Px0eYTAnK5apnIskX3eG7_FzerM/exec?source=%s&target=%s&q=%s";
+
+    @Override
+    protected void updateTranslationResult(TranslationResult translationResult,
+        String sourceLanguage, String targetLanguage, String text)
+        throws URISyntaxException, IOException, InterruptedException
+    {
+        String url = String.format(GOOGLE_SCRIPTS_URL_FORMAT,
+            sourceLanguage, targetLanguage, encode(text));
+
+        String content = getContent(url);
+
+        if (content.startsWith("<!DOCTYPE html>")) {
+            throw new IOException("Google scripts' exception has occurred");
+        }
+
+        translationResult.setTranslation(getContent(url));
+    }
+}
