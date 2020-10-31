274e0d48114f0d4473fc4e2a3d1b5ae4c6c3dc40
omarkelov <36790281+omarkelov@users.noreply.github.com>
2020-10-31 17:46:13 +0700

    #17 GoogleJsonTranslator: parsing objects instead of strings

--- JavaFXPlayer/src/main/java/ru/nsu/fit/markelov/translation/googlejson/GoogleJsonTranslator.java
+++ JavaFXPlayer/src/main/java/ru/nsu/fit/markelov/translation/googlejson/GoogleJsonTranslator.java
@@ -73,20 +73,16 @@ public class GoogleJsonTranslator extends Translator {
 
     private void updateTranslationResultFromJson(TranslationResult translationResult, String json) {
         try {
-            //json = "[[[\"прекрасный\",\"lovely\",null,null,3,null,null,[[]],[[[\"cce7c67b3f2439089dd6b428e0b83b88\",\"en_ru_2020q2.md\"]]]],[null,null,\"prekrasnyy\",\"ˈləvlē\"]],[[\"имя прилагательное\",[\"прекрасный\",\"красивый\",\"милый\",\"восхитительный\",\"очаровательный\",\"прелестный\",\"чудный\",\"привлекательный\"],[[\"прекрасный\",[\"beautiful\",\"fine\",\"great\",\"glorious\",\"lovely\",\"beauteous\"],null,0.17929012],[\"красивый\",[\"beautiful\",\"handsome\",\"nice\",\"lovely\",\"goodly\",\"fair\"],null,0.038774207],[\"милый\",[\"cute\",\"dear\",\"nice\",\"sweet\",\"lovely\",\"darling\"],null,0.01720595],[\"восхитительный\",[\"delightful\",\"delicious\",\"lovely\",\"adorable\",\"delectable\",\"admirable\"],null,0.00800151],[\"очаровательный\",[\"charming\",\"enchanting\",\"fascinating\",\"glamorous\",\"captivating\",\"lovely\"],null,0.0071725072],[\"прелестный\",[\"charming\",\"lovely\",\"pretty\",\"adorable\",\"exquisite\",\"delectable\"],null,0.003084857],[\"чудный\",[\"marvelous\",\"wonderful\",\"lovely\",\"dreamy\",\"marvellous\"],null,0.0012080474],[\"привлекательный\",[\"attractive\",\"appealing\",\"engaging\",\"inviting\",\"lovable\",\"lovely\"],null,0.00050358905]],\"lovely\",3],[\"имя существительное\",[\"красотка\"],[[\"красотка\",[\"babe\",\"beautiful\",\"cutie\",\"lovely\",\"bombshell\",\"peach\"],null,0.00036843363,null,2]],\"lovely\",1]],\"en\",null,null,[[\"lovely\",null,[[\"прекрасный\",0,true,false]],[[0,6]],\"lovely\",0,0]],0.46797875,[],[[\"en\"],null,[0.46797875],[\"en\"]],null,null,[[\"имя прилагательное\",[[[\"beauteous\"],\"m_en_gbus0597020.007\"],[[\"sightly\",\"pulchritudinous\"],\"m_en_gbus0597020.007\"],[[\"tasty\",\"knockout\",\"stunning\",\"drop-dead gorgeous\",\"adorbs\",\"fit\",\"smashing\",\"cute\",\"foxy\"],\"m_en_gbus0597020.007\"],[[\"comely\",\"fair\"],\"m_en_gbus0597020.007\"],[[\"beautiful\",\"pretty\",\"as pretty as a picture\",\"attractive\",\"good-looking\",\"appealing\",\"handsome\",\"adorable\",\"exquisite\",\"sweet\",\"personable\",\"charming\",\"enchanting\",\"engaging\",\"bewitching\",\"winsome\",\"seductive\",\"gorgeous\",\"alluring\",\"ravishing\",\"glamorous\",\"bonny\"],\"m_en_gbus0597020.007\"],[[\"scenic\",\"picturesque\",\"pleasing\",\"easy on the eye\",\"magnificent\",\"stunning\",\"splendid\"],\"m_en_gbus0597020.007\"],[[\"delightful\",\"very pleasant\",\"very nice\",\"very agreeable\",\"marvelous\",\"wonderful\",\"sublime\",\"superb\",\"fine\",\"magical\",\"enchanting\",\"captivating\",\"terrific\",\"fabulous\",\"fab\",\"heavenly\",\"divine\",\"amazing\",\"glorious\"],\"m_en_gbus0597020.011\"]],\"lovely\"]],[[\"имя прилагательное\",[[\"exquisitely beautiful.\",\"m_en_gbus0597020.007\",\"lovely views\"]],\"lovely\"],[\"имя существительное\",[[\"a glamorous woman or girl.\",\"m_en_gbus0597020.013\",\"a bevy of rock lovelies\"]],\"lovely\"]],[[[\"a <b>lovely</b> house\",null,null,null,3,\"neid_12170\"],[\"<b>lovely</b> views\",null,null,null,3,\"m_en_gbus0597020.007\"],[\"he's a <b>lovely</b> little boy\",null,null,null,3,\"neid_12172\"],[\"I got a <b>lovely</b> letter today\",null,null,null,3,\"neid_12173\"],[\"we've had a <b>lovely</b> day\",null,null,null,3,\"m_en_gbus0597020.011\"],[\"that suit looks <b>lovely</b> on you\",null,null,null,3,\"neid_12170\"],[\"<b>lovely</b>, thanks!\",null,null,null,3,\"neid_12169\"],[\"that's just <b>lovely</b>!\",null,null,null,3,\"neid_12174\"],[\"she's a <b>lovely</b> person\",null,null,null,3,\"m_en_gbus0597020.011\"],[\"you have <b>lovely</b> eyes\",null,null,null,3,\"m_en_gbus0597020.007\"],[\"we had a <b>lovely</b> day\",null,null,null,3,\"neid_12171\"],[\"it was <b>lovely</b> to see him again\",null,null,null,3,\"neid_12171\"]]]]"; // todo delete
-
             // trying to set mainTranslation
             JSONArray outerArray = new JSONArray(json);
-            JSONArray mainArray = new JSONArray(outerArray.get(0).toString());
+            JSONArray mainArray = outerArray.getJSONArray(0);
 
             StringBuilder translationBuilder = new StringBuilder();
             for (int i = 0; i < mainArray.length(); i++) {
-                JSONArray mainTranslationArray = new JSONArray(mainArray.get(i).toString());
-                String translationPart = mainTranslationArray.get(0).toString(); // todo! parse string from object
+                JSONArray mainTranslationArray = mainArray.getJSONArray(i);
+                String translationPart = mainTranslationArray.optString(0);
 
-                if (!translationPart.equals("null")) {
-                    translationBuilder.append(translationPart);
-                }
+                translationBuilder.append(translationPart);
             }
 
             if (translationBuilder.length() == 0) {
@@ -96,26 +92,26 @@ public class GoogleJsonTranslator extends Translator {
             translationResult.setTranslation(translationBuilder.toString());
 
             // trying to set translationGroups
-            JSONArray groupsArray = new JSONArray(outerArray.get(1).toString());
+            JSONArray groupsArray = outerArray.getJSONArray(1);
             List<TranslationGroup> translationGroups = new ArrayList<>();
             for (int i = 0; i < groupsArray.length(); i++) {
-                JSONArray groupArray = new JSONArray(groupsArray.get(i).toString());
+                JSONArray groupArray = groupsArray.getJSONArray(i);
 
                 TranslationGroup translationGroup = new TranslationGroup();
-                translationGroup.setPartOfSpeech(groupArray.get(0).toString());
+                translationGroup.setPartOfSpeech(groupArray.optString(0));
 
-                JSONArray variantsArray = new JSONArray(groupArray.get(2).toString());
+                JSONArray variantsArray = groupArray.getJSONArray(2);
                 List<TranslationVariant> translationVariants = new ArrayList<>();
                 for (int j = 0; j < variantsArray.length(); j++) {
-                    JSONArray variantArray = new JSONArray(variantsArray.get(j).toString());
+                    JSONArray variantArray = variantsArray.getJSONArray(j);
 
                     TranslationVariant translationVariant = new TranslationVariant();
-                    translationVariant.setWord(variantArray.get(0).toString());
+                    translationVariant.setWord(variantArray.optString(0));
 
-                    JSONArray translationArray = new JSONArray(variantArray.get(1).toString());
+                    JSONArray translationArray = variantArray.getJSONArray(1);
                     List<String> translations = new ArrayList<>();
                     for (int k = 0; k < translationArray.length(); k++) {
-                        translations.add(translationArray.get(k).toString());
+                        translations.add(translationArray.optString(k));
                     }
                     translationVariant.setTranslations(translations);
 
