9ce070ac57ae7e81763604382e8ef32f1de136db
omarkelov <36790281+omarkelov@users.noreply.github.com>
2020-11-01 17:32:24 +0700

    #17 TranslationResult: removed toString()

--- JavaFXPlayer/src/main/java/ru/nsu/fit/markelov/translation/entities/TranslationResult.java
+++ JavaFXPlayer/src/main/java/ru/nsu/fit/markelov/translation/entities/TranslationResult.java
@@ -1,36 +1,11 @@
 package ru.nsu.fit.markelov.translation.entities;
 
 import java.util.List;
-import java.util.StringJoiner;
 
 public class TranslationResult {
 
     private String translation;
     private List<TranslationGroup> translationGroups;
-
-    @Override
-    public String toString() {
-        if (translationGroups != null) {
-            StringJoiner partOfSpeechJoiner = new StringJoiner(System.lineSeparator());
-            for (TranslationGroup translationGroup : translationGroups) {
-                StringJoiner variantJoiner = new StringJoiner(System.lineSeparator() + "  ", translationGroup.getPartOfSpeech() + System.lineSeparator() + "  ", "");
-                for (TranslationVariant translationVariant : translationGroup.getVariants()) {
-                    StringJoiner wordJoiner = new StringJoiner(", ", translationVariant.getWord() + " (", ")");
-                    for (String translation : translationVariant.getTranslations()) {
-                        wordJoiner.add(translation);
-                    }
-                    variantJoiner.add(wordJoiner.toString());
-                }
-                partOfSpeechJoiner.add(variantJoiner.toString());
-            }
-
-            return partOfSpeechJoiner.toString();
-        } else if (translation != null) {
-            return translation;
-        } else {
-            return "No translation...";
-        }
-    }
 
     public boolean isEmpty() {
         return translation == null && translationGroups == null;
