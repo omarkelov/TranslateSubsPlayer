4a02d163b851a87c627024c3c1b1d380f433b772
omarkelov <36790281+omarkelov@users.noreply.github.com>
2020-11-03 13:57:29 +0700

    #11 Created global Constants

---
+++ JavaFXPlayer/src/main/java/ru/nsu/fit/markelov/Constants.java
@@ -0,0 +1,15 @@
+package ru.nsu.fit.markelov;
+
+import javafx.scene.text.Font;
+
+public class Constants {
+    public static final String NEW_LINE = System.lineSeparator();
+    public static final String SPACE = " ";
+
+    public static final String VLC_PRETTY_URI = "videolan.org";
+    public static final String VLC_URI = "https://www.videolan.org/";
+
+    public static final Font SMALL_FONT = new Font(10);
+    public static final Font MEDIUM_FONT = new Font(20);
+    public static final Font BIG_FONT = new Font(30);
+}
