92e2d2ad4f5364a13e81618fb2c937bf1404333e
pkaraseva18214 <karasyova_polina@mail.ru>
2020-12-09 19:13:17 +0700

    #21 Приблизительный внешний вид веб-страниц

--- Web-client/Registration and log in/Webserver/src/main/java/com/web/server/repository/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.web.server.repository;
-
-import com.web.server.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long>{
-}
--- Web-client/Registration and log in/Webserver/src/main/java/com/web/server/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.web.server.repository;
-
-import com.web.server.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-  User findByUsername(String username);
-}
