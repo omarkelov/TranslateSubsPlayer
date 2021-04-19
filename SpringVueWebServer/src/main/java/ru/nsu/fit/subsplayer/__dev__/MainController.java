40a9676a7f7559ef8a29c3dffb1b02057d0d4b29
omarkelov <36790281+omarkelov@users.noreply.github.com>
2021-04-19 19:58:15 +0700

    #23 Add index page demo

---
+++ SpringVueWebServer/src/main/java/ru/nsu/fit/subsplayer/__dev__/MainController.java
@@ -0,0 +1,16 @@
+package ru.nsu.fit.subsplayer.__dev__;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Map;
+
+@Controller
+public class MainController {
+    @GetMapping("/index")
+    public String index(Map<String, Object> model, @AuthenticationPrincipal UserDetails user) {
+        return "index";
+    }
+}
