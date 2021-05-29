7b1624d93b648434500a20e3573b680c777d968f
omarkelov <36790281+omarkelov@users.noreply.github.com>
2021-05-29 15:49:48 +0700

    #25 Create VideoEditor

---
+++ JavaFXPlayer/src/main/java/ru/nsu/fit/markelov/video/VideoEditor.java
@@ -0,0 +1,47 @@
+package ru.nsu.fit.markelov.video;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+public class VideoEditor {
+
+    private static final boolean IS_WINDOWS =
+        System.getProperty("os.name").toLowerCase().startsWith("windows");
+
+    private static final File FFMPEG_DIRECTORY = new File("C:\\ffmpeg\\bin\\"); // todo -hardcode
+
+    public static String cutVideo(String videoFilePath, int audioChannel,
+                                  long startTime, long endTime) {
+        try {
+            String newVideoFileName = UUID.randomUUID() + ".mp4";
+
+            String command = String.format(
+                "ffmpeg -ss %dms -i \"%s\" -to %dms -c copy -map 0:v:0 -map 0:a:%d %s",
+                startTime, videoFilePath, endTime - startTime, audioChannel, newVideoFileName);
+            System.out.println(command);
+
+            ProcessBuilder processBuilder = new ProcessBuilder();
+            processBuilder.directory(FFMPEG_DIRECTORY);
+
+            if (IS_WINDOWS) {
+                processBuilder.command("cmd.exe", "/c", command);
+            } else {
+                processBuilder.command("sh", "-c", command);
+            }
+
+            Process process = processBuilder.start();
+            int exitCode = process.waitFor();
+            process.destroy();
+            if (exitCode != 0) {
+                System.err.println("Bad exitCode: " + exitCode);
+                return null;
+            }
+
+            return FFMPEG_DIRECTORY + newVideoFileName;
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
